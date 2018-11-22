94c1f4995f2bc3c13535c0fed2154344395dbdcb
Mohab999 <mohab@gmail.com>
2018-11-22 13:21:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,10 +7,10 @@ client.on('ready', () => {
 });
 
 client.on('ready', async() => {
-var server = "509357857666236430"; // ايدي السررفر
-var channel = "509357857666236434";//ايدي الروم
+var server = "514839663589064724"; // ايدي السررفر
+var channel = "514840054405791745";//ايدي الروم
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
+    client.guilds.get(server).channels.get(channel).send('**BOX Shop , BOX Shop , BOX Shop , BOX Shop , BOX Shop , BOX Shop , BOX Shop , BOX Shop , BOX Shop , BOX Shop , BOX Shop , BOX Shop , BOX Shop , BOX Shop , BOX Shop , BOX Shop , BOX Shop , BOX Shop , **')
     },305);
 })
 
